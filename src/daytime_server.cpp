9516f0ee5b06a151ce977e662f4d0b38b5757490
Peter Laust Almvig <petealmvig@gmail.com>
2023-03-12 14:48:38 +0100

    Simon han er udm

--- src/daytime_server.cpp
+++ src/daytime_server.cpp
@@ -15,10 +15,15 @@
 
 using boost::asio::ip::tcp;
 
+//Get the current date and time.
 std::string make_daytime_string()
 {
   using namespace std; // For time_t, time and ctime;
+
+  //get the current time as a time_t object represented as unix epoch time
   time_t now = time(0);
+
+  //Convert to readable format and return
   return ctime(&now);
 }
 
@@ -28,16 +33,23 @@ int main()
   {
     boost::asio::io_context io_context;
 
+    //Allocate the socket with ipv4 and on port 13 commonly used for daytime services.
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
 
     while (true)
     {
+      //Open socket
       tcp::socket socket(io_context);
+
+      //accepts a new connection on the socket if it finds it.
       acceptor.accept(socket);
 
+      //Read the time of day
       std::string message = make_daytime_string();
 
       boost::system::error_code ignored_error;
+
+      //Write to the socket. This is the data sent to the client.
       boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
     }
   }
